4647482a9647684a40370b8e4b28d658119c3dbf
Sam Miller <sm1215@gmail.com>
2017-06-25 01:33:26 -0400

    added mouse tracking

--- random-movements/logic.js
+++ random-movements/logic.js
@@ -6,35 +6,67 @@ var creative = {
   radius: 40,
   circleLimit: 50,
   circles: [],
+  tracking: false,
 
   initialize: function() {
     this.setDom();
     this.setBounds();
+    this.setListeners();
     this.construct();
     this.animate();
   },
 
   setDom: function() {
     this.main = document.querySelector('#main-container');
+    this.feature = document.querySelector('#feature');
     this.svg = document.querySelector('svg');
   },
 
   setBounds: function(){
-
-    TweenMax.to(this.main, this.speed, {
+    TweenMax.set(this.main, {
       width: this.width,
       height: this.height,
       top: this.height / 4,
       opacity: 1
     });
 
-    TweenMax.to(this.svg, this.speed, {
+    TweenMax.set(this.svg, {
       attr: {
         width: this.width,
         height: this.height,
         viewBox: `0 0 ${this.width} ${this.height}`
       },
       opacity: 1
+    });
+  },
+
+  setListeners: function(){
+    window.addEventListener('mousemove', (e) => {
+      var cx = e.clientX;
+      var cy = e.clientY;
+      var featureBounds = function(){
+        return this.feature.getBoundingClientRect();
+      }
+      this.cx = cx - featureBounds().left;
+      this.cy = cy - featureBounds().top;
+      if(
+        (cx > featureBounds().left && cy > featureBounds().top) &&
+        (cx < featureBounds().right && cy > featureBounds().top) &&
+        (cx < featureBounds().right && cy < featureBounds().bottom) &&
+        (cx > featureBounds().left && cy < featureBounds().bottom)
+      ){
+        this.tracking = true;
+        TweenMax.set(this.svg, {
+          borderRadius: '100%',
+          backgroundColor: 'rgba(250,0,0,0.1)'
+        });
+      } else {
+        this.tracking = false;
+        TweenMax.set(this.svg, {
+          borderRadius: '100%',
+          backgroundColor: 'transparent'
+        });
+      }
     });
   },
 
@@ -65,14 +97,30 @@ var creative = {
 
   animate: function(){
 
+    var user = function(){
+      return attr = {
+        cx: this.creative.cx,
+        cy: this.creative.cy,
+        r: _.random(0, this.creative.radius, false)
+      }
+    }
+
+    var random = function(){
+      return attr = {
+        cx: _.random(this.creative.radius, this.creative.width - this.creative.radius, false),
+        cy: _.random(this.creative.radius, this.creative.height - this.creative.radius, false),
+        r: _.random(0, this.creative.radius, false)
+      }
+    }
+
+    var getMoveType = function(){
+      return this.creative.tracking ? user() : random();
+    }
+
     function move(circle){
       TweenMax.to(circle, this.creative.speed, {
         fill: this.creative.generateColor(),
-        attr: {
-          cx: _.random(this.creative.radius, this.creative.width - this.creative.radius, false),
-          cy: _.random(this.creative.radius, this.creative.height - this.creative.radius, false),
-          r: _.random(0, this.creative.radius, false),
-        },
+        attr: getMoveType(),
         ease: Power0.easeNone,
         onComplete: move,
         onCompleteParams: [circle],
