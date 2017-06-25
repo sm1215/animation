4794f1ec267fac554a234ca249bc42deca19f1ff
Sam Miller <sm1215@gmail.com>
2017-06-24 21:56:37 -0400

    setting up repeating animation that generates new values on each iteration

---
+++ random-movements/logic.js
@@ -0,0 +1,81 @@
+var creative = {
+  svgns: "http://www.w3.org/2000/svg",
+  width: 500,
+  height: 500,
+  speed: 2,
+  radius: 20,
+  circleLimit: 10,
+  circles: [],
+
+  initialize: function() {
+    this.setDom();
+    this.setBounds();
+    this.construct();
+    this.animate();
+  },
+
+  setDom: function() {
+    this.main = document.querySelector('#main-container');
+    this.svg = document.querySelector('svg');
+  },
+
+  setBounds: function(){
+    TweenMax.set(this.main, {
+      width: this.width,
+      height: this.height
+    });
+    TweenMax.set(this.svg, {
+      attr: {
+        width: this.width,
+        height: this.height,
+        viewBox: '0 0 '+ this.width +' '+ this.height
+      }
+    });
+  },
+
+  construct: function(){
+    _.times(this.circleLimit, () => {
+      var el = document.createElementNS(this.svgns, 'circle');
+
+      TweenMax.set(el, {
+        attr: {
+          cx: 0,
+          cy: 0,
+          r: this.radius          
+        }
+      });
+
+      this.circles.push(el);
+      this.svg.appendChild(el);
+    });
+  },
+
+  generateColor: function(){
+    var values = [];
+    _.times(3, () => {
+      values.push(_.random(0, 255, false));
+    });
+    values.push(_.random(0.2,1));
+    return `rgba(${values.join(',')})`;
+  },
+
+  animate: function(){
+    function move(circle){
+      TweenMax.to(circle, this.creative.speed, {
+        fill: this.creative.generateColor(),
+        attr: {
+          cx: _.random(0, this.creative.width, false),
+          cy: _.random(0, this.creative.height, false)
+        },
+        ease: Power0.easeNone,
+        onComplete: move,
+        onCompleteParams: [circle],
+        onCompleteScope: this
+      });
+    }
+
+    _.forEach(this.circles, (circle) => {
+      move(circle);
+    });
+  }
+}
